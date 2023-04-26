34de4c65e0af7eddb4e52902c585d8d65226b82a
Bartosz.Jagodzinski <bartosz.jagodzinski@asseco.pl>
2023-04-26 21:40:19 +0200

    name changes

--- src/main/resources/liquibase/data/2-initialData.sql
+++ src/main/resources/liquibase/data/2-initialData.sql
@@ -1,11 +1,11 @@
 INSERT INTO public.person (id, first_name, last_name)
-VALUES (nextval('person_seq'), 'Daniel'::varchar(255), 'Kubik'::varchar(255));
+VALUES (nextval('person_seq'), 'Daniel'::varchar(255), 'Kowalski'::varchar(255));
 
 INSERT INTO public.person (id, first_name, last_name)
-VALUES (nextval('person_seq'), 'Marek'::varchar(255), 'Wojcieszek'::varchar(255));
+VALUES (nextval('person_seq'), 'Marek'::varchar(255), 'Nowak'::varchar(255));
 
 INSERT INTO public.person (id, first_name, last_name)
-VALUES (nextval('person_seq'), 'Bartosz'::varchar(255), 'Jagodziński'::varchar(255));
+VALUES (nextval('person_seq'), 'Bartosz'::varchar(255), 'Baran'::varchar(255));
 
 INSERT INTO public.permission (id, name)
 VALUES (1, 'Permission1'::varchar(255));
